c7c5622d5db0effc25d977effd92014826c6e982
Shane Takeuchi <shanetakeuchi19@gmail.com>
2022-04-26 13:49:48 -0500

    start of locks

--- kernel/boot.c
+++ kernel/boot.c
@@ -26,9 +26,8 @@ static uint8_t stack[8192];
 
 // Request that the beginning of memory (address 0x0) be unmapped
 static struct stivale2_tag unmap_null_hdr_tag = {
-  .identifier = STIVALE2_HEADER_TAG_UNMAP_NULL_ID,
-  .next = 0
-};
+    .identifier = STIVALE2_HEADER_TAG_UNMAP_NULL_ID,
+    .next = 0};
 
 // Request a terminal from the bootloader
 static struct stivale2_header_tag_terminal terminal_hdr_tag = {
@@ -61,7 +60,8 @@ __attribute__((section(".stivale2hdr"), used)) static struct stivale2_header sti
     .tags = (uintptr_t)&smp_tag};
 
 // Find a tag with a given ID
-void *find_tag(struct stivale2_struct *hdr, uint64_t id) {
+void *find_tag(struct stivale2_struct *hdr, uint64_t id)
+{
   // Start at the first tag
   struct stivale2_tag *current = (struct stivale2_tag *)hdr->tags;
 
@@ -82,8 +82,8 @@ void *find_tag(struct stivale2_struct *hdr, uint64_t id) {
   return NULL;
 }
 
-
-void term_setup(struct stivale2_struct *hdr) {
+void term_setup(struct stivale2_struct *hdr)
+{
   // Look for a terminal tag
   struct stivale2_struct_tag_terminal *tag = find_tag(hdr, STIVALE2_STRUCT_TAG_TERMINAL_ID);
 
@@ -95,17 +95,46 @@ void term_setup(struct stivale2_struct *hdr) {
   // set_term_write((term_write_t)tag->term_write);
 }
 
+int lock_int = 1;
 
-void func() {
-  kprintf("hello world\n");
-  int val = 1;
-  if (val) {
-    val = 0;
-    void *here = ADDRESS_HERE();
-    // kprintf("core: %d, address: %p\n", ADDRESS_HERE());
-  } else {
-    kprintf("ppppppp\n");
-  }
+typedef struct lock_struct
+{
+  int num_locks;
+} lock_t;
+int my_lock = 1;
+int expected = 1;
+
+void lock(lock_t *lock)
+{
+  // __atomic_compare_exchange_n(type * ptr, type * expected, type desired, bool weak, int success_memorder, int failure_memorder) return 1;
+  while (__atomic_compare_exchange_n(&my_lock, &expected, 0, false, __ATOMIC_SEQ_CST, 0) == 0)
+    ;
+}
+
+void unlock(lock_t *lock)
+{
+  lock->num_locks = 1;
+}
+
+lock_t our_lock = {.num_locks = 1};
+
+void func()
+{
+  lock(&our_lock);
+  // kprintf("hello world\n");
+  kprintf("%d\n", our_lock.num_locks);
+  unlock(&our_lock);
+  // int val = 1;
+  // if (val)
+  // {
+  //   val = 0;
+  //   void *here = ADDRESS_HERE();
+  //   // kprintf("core: %d, address: %p\n", ADDRESS_HERE());
+  // }
+  // else
+  // {
+  //   kprintf("ppppppp\n");
+  // }
   halt();
 }
 
@@ -114,7 +143,8 @@ void func2()
   halt();
 }
 
-void _start(struct stivale2_struct *hdr) {
+void _start(struct stivale2_struct *hdr)
+{
   // We've booted! Let's start processing tags passed to use from the bootloader
   term_setup(hdr);
 
@@ -136,7 +166,6 @@ void _start(struct stivale2_struct *hdr) {
   // Initialize the terminal
   term_init();
 
-
   // Set up keyboard interrupts
   pic_init();
   pic_unmask_irq(1);
@@ -144,6 +173,8 @@ void _start(struct stivale2_struct *hdr) {
   // Unmap the lower half of memory
   uintptr_t root = read_cr3() & 0xFFFFFFFFFFFFF000;
   unmap_lower_half(root);
+
+  kprintf("hi: %d\n", our_lock.num_locks);
 
   for (int i = 0; i < smp->cpu_count; i++)
   {
@@ -162,16 +193,21 @@ void _start(struct stivale2_struct *hdr) {
 
   for (int i = 1; i < smp->cpu_count; i++)
   {
-    if (i == 1) {
+    // if (1)
+    // {
 
-      smp->smp_info[i].extra_argument = i;
-      smp->smp_info[i].goto_address = (uint64_t)(func);
-    } else if (i == 2){
-      // for (int i = 0; i < 0xf0000000;i++){};
-      // smp->smp_info[i].goto_address = (uint64_t)(func);
-    } else {
-      smp->smp_info[i].goto_address = (uint64_t)(func2);
-    }
+    smp->smp_info[i].extra_argument = i;
+    smp->smp_info[i].goto_address = (uint64_t)(func);
+    // }
+    // else if (i == 2)
+    // {
+    //   // for (int i = 0; i < 0xf0000000;i++){};
+    //   // smp->smp_info[i].goto_address = (uint64_t)(func);
+    // }
+    // else
+    // {
+    //   smp->smp_info[i].goto_address = (uint64_t)(func2);
+    // }
   }
 
   // Get information about the modules we've asked the bootloader to load
@@ -179,12 +215,12 @@ void _start(struct stivale2_struct *hdr) {
   // Save information about the modules to be accessed later when we make an exec system call
   module_setup(modules);
 
-
   kprintf("number of tags: %d\n", smp->cpu_count);
   // Launch the init program
   for (int i = 0; i < modules->module_count; i++)
   {
-    if (!strcmp(modules->modules[i].string, "init")) {
+    if (!strcmp(modules->modules[i].string, "init"))
+    {
       run_program(modules->modules[i].begin);
     }
   }
